8fd1b89fc2c965d9025509a7a490a8aece5b88a2
j13000355 <julienjandot@orange.fr>
2016-11-09 16:55:56 +0100

    ajout timer

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,10 @@
 public class Application {
 
     public static void main(String[] args) {
-
+        long initTime = System.nanoTime();
+        Levenshtein levenshtein = new Levenshtein("logarytmique", "algorithmique");
+        System.out.println(levenshtein.getDistance());
+        long endTime = System.nanoTime();
+        System.out.println("Temps algo : " + (endTime - initTime) + " nanosecondes.");
     }
 }
--- src/main/java/Levenshtein.java
+++ src/main/java/Levenshtein.java
@@ -12,16 +12,6 @@ public class Levenshtein {
         distance = 0;
     }
 
-    private String balanceString(int nbSpaces, String word){
-        System.out.println(word);
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < nbSpaces; ++i){
-            sb.append(" ");
-        }
-        sb.append(word);
-        return sb.toString();
-    }
-
     private void swap(){
         String tmp;
         tmp = mot1;
@@ -30,16 +20,15 @@ public class Levenshtein {
     }
 
     public int getDistance(){
-        if (mot1.length() == 0) return distance;
-
         if(mot1.length() > mot2.length())
             swap();
+
+        if (mot1.length() == 0) return distance;
 
         char lastChar1 = mot1.charAt(mot1.length()-1);
         char lastChar2 = mot2.charAt(mot2.length()-1);
 
         if (lastChar1 == lastChar2) {
-            System.out.println("==");
             mot1 = mot1.substring(0, mot1.length() - 1);
             mot2 = mot2.substring(0, mot2.length() - 1);
             getDistance();
@@ -47,14 +36,11 @@ public class Levenshtein {
         else {
             System.out.println(mot1 + " != " + mot2);
             if (mot1.length() < mot2.length() && mot1.indexOf(lastChar2) == -1) {
-                System.out.println("add");
                 mot1 += lastChar2;
                 System.out.println(mot1);
             }
-            else {
+            else
                 mot1 = mot1.replace(lastChar1, lastChar2);
-                System.out.println("replace");
-            }
             ++distance;
             System.out.println(distance);
             getDistance();
